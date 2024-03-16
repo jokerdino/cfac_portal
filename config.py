9fb61537621e1a10b3d858e111d5b9bf3162fa7d
Barneedhar Vigneshwar <barneedhar@gmail.com>
2024-03-16 17:17:25 +0530

    Increase pagelength to 100 in coinsurance balances page

--- config.py
+++ config.py
@@ -2,7 +2,9 @@ import os
 
 
 class Config:
-    SECRET_KEY = "secret"  # os.urandom(32)
+    # SECRET_KEY = os.urandom(32)
+    SECRET_KEY = "secret"
+
     WTF_CSRF_ENABLED = True
     SQLALCHEMY_DATABASE_URI = (
         "postgresql://cfac_user:cfac_user@localhost:5432/cfac_portal"
