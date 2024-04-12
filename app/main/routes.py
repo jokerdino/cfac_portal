3fd911173f05da62d63e15625672c711727be6cd
Barneedhar Vigneshwar <barneedhar@gmail.com>
2024-04-12 10:51:13 +0530

    Enable POS dashboard link in homepage

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,5 +1,5 @@
 from flask import redirect, render_template, url_for
-from flask_login import current_user
+from flask_login import current_user, login_required
 
 from app.main import main_bp
 
@@ -10,3 +10,9 @@ def index():
         return render_template("index.html")
     else:
         return redirect(url_for("users.login_page"))
+
+
+@main_bp.route("/pos")
+@login_required
+def pos_dashboard():
+    return render_template("pos_dashboard.html")
