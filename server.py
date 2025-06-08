c11b16a8f606b7e6219b096588ed90d3a9f85589
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-06-08 12:59:04 +0530

    Reinsurance: Added changes to server.py file

--- server.py
+++ server.py
@@ -39,7 +39,8 @@ from app.mis_tracker import mis_bp
 
 from app.brs_centralised_cheque import brs_cc_bp
 from app.correspondence import correspondence_bp
-from app.ri_page import ri_page_bp
+
+from app.reinsurance import reinsurance_bp
 
 from app.errors import errors_bp
 from app.cfac_flask_admin import flask_admin_bp
@@ -224,7 +225,8 @@ def create_app(config_class=Config):
     app.register_blueprint(brs_cc_bp, url_prefix="/brs_cc")
     app.register_blueprint(em_bp, url_prefix="/escalation_matrix")
     app.register_blueprint(correspondence_bp, url_prefix="/correspondence")
-    app.register_blueprint(ri_page_bp, url_prefix="/ri")
+
+    app.register_blueprint(reinsurance_bp, url_prefix="/reinsurance")
 
     app.register_blueprint(errors_bp, url_prefix="/error")
     app.register_blueprint(flask_admin_bp, url_prefix="/admin")
