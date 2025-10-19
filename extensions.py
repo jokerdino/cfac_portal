add45a1361ef36fef0961641f14e352ac9bff6a3
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-10-19 11:41:32 +0530

    Set up Base Class for sqlalchemy

--- extensions.py
+++ extensions.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase
 from flask_debugtoolbar import DebugToolbarExtension
 
 from flask_admin_models import MyAdminIndexView
@@ -11,17 +12,21 @@ from flask_admin_models import MyAdminIndexView
 migrate = Migrate(compare_type=True)
 lm = LoginManager()
 
-convention = {
-    "ix": "ix_%(column_0_label)s",
-    "uq": "uq_%(table_name)s_%(column_0_name)s",
-    "ck": "ck_%(table_name)s_%(constraint_name)s",
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-    "pk": "pk_%(table_name)s",
-}
 
-metadata = MetaData(naming_convention=convention)
+class Base(DeclarativeBase):
+    metadata = MetaData(
+        naming_convention={
+            "ix": "ix_%(column_0_label)s",
+            "uq": "uq_%(table_name)s_%(column_0_name)s",
+            "ck": "ck_%(table_name)s_%(constraint_name)s",
+            "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+            "pk": "pk_%(table_name)s",
+        }
+    )
 
-db = SQLAlchemy(metadata=metadata)
+
+db = SQLAlchemy(model_class=Base)
+
 
 admin = Admin(
     theme=Bootstrap4Theme(fluid=True, swatch="united"),
