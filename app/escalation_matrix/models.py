569b6b9df81aa9893c54541fe012e19ef62dc714
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-10-20 17:44:06 +0530

    Escalation matrix: Updated models to use v2.0 style

--- app/escalation_matrix/models.py
+++ app/escalation_matrix/models.py
@@ -1,22 +1,28 @@
 from datetime import datetime
+from typing import Optional
+
+from sqlalchemy.orm import Mapped, mapped_column
 from flask_login import current_user
+
 from extensions import db
 
 
 class EscalationMatrix(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True)
 
-    service_type = db.Column(db.String)
-    nature_of_entity = db.Column(db.String)
-    name_of_entity = db.Column(db.String)
-    level = db.Column(db.String)
-    name = db.Column(db.String)
-    roll = db.Column(db.String)
-    email_address = db.Column(db.String)
-    contact_number = db.Column(db.String)
+    service_type: Mapped[str]
+    nature_of_entity: Mapped[str]
+    name_of_entity: Mapped[str]
+    level: Mapped[str]
+    name: Mapped[str]
+    roll: Mapped[str]
+    email_address: Mapped[str]
+    contact_number: Mapped[str]
 
-    created_by = db.Column(db.String, default=lambda: current_user.username)
-    created_on = db.Column(db.DateTime, default=datetime.now)
+    created_by: Mapped[str] = mapped_column(default=lambda: current_user.username)
+    created_on: Mapped[datetime] = mapped_column(default=datetime.now)
 
-    updated_by = db.Column(db.String, onupdate=lambda: current_user.username)
-    updated_on = db.Column(db.DateTime, onupdate=datetime.now)
+    updated_by: Mapped[Optional[str]] = mapped_column(
+        onupdate=lambda: current_user.username
+    )
+    updated_on: Mapped[Optional[datetime]] = mapped_column(onupdate=datetime.now)
