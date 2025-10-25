7b2d2a14eb197faa5812d4383622150aa83abe34
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-10-25 20:43:07 +0530

    Added oo_user_only view permission

--- set_view_permissions.py
+++ set_view_permissions.py
@@ -28,6 +28,18 @@ def ro_user_only(f):
     return wrap
 
 
+def oo_user_only(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if current_user.user_type in ["admin", "ro_user", "oo_user"]:
+            return f(*args, **kwargs)
+        else:
+            # flash("You need to be an admin to view this page")
+            return redirect(url_for("main.index"))
+
+    return wrap
+
+
 def fund_managers(f):
     @wraps(f)
     def wrap(*args, **kwargs):
