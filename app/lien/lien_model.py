4557d281492331c28ae234d58d53a362b85337cf
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-11-09 21:49:21 +0530

    Lien: Update models to sqlalchemy v2.0 style

--- app/lien/lien_model.py
+++ app/lien/lien_model.py
@@ -1,12 +1,12 @@
-from datetime import datetime
+from datetime import date
+from typing import Optional
 
-from sqlalchemy.orm import validates
+from sqlalchemy.orm import validates, mapped_column, Mapped
 from sqlalchemy_continuum.plugins import FlaskPlugin
 from sqlalchemy_continuum import make_versioned
-from flask_login import current_user
 
 
-from extensions import db
+from extensions import db, IntPK, CreatedBy, CreatedOn, UpdatedBy, UpdatedOn
 
 from .lien_forms import ALLOWED_RO_CODES
 
@@ -15,61 +15,61 @@ make_versioned(plugins=[FlaskPlugin()])
 
 class Lien(db.Model):
     __versioned__ = {}
-    id = db.Column(db.Integer, primary_key=True)
+    id: Mapped[IntPK]
 
-    bank_name = db.Column(db.String)
-    account_number = db.Column(db.String)
+    bank_name: Mapped[Optional[str]]
+    account_number: Mapped[Optional[str]]
 
-    ro_name = db.Column(db.String)
-    ro_code = db.Column(db.String)
-    lien_amount = db.Column(db.Numeric(15, 2))
-    lien_date = db.Column(db.Date)
-    court_order_lien = db.Column(db.String)
+    ro_name: Mapped[Optional[str]]
+    ro_code: Mapped[Optional[str]]
+    lien_amount: Mapped[Optional[float]] = mapped_column(db.Numeric(15, 2))
+    lien_date: Mapped[Optional[date]]
+    court_order_lien: Mapped[Optional[str]]
 
-    dd_amount = db.Column(db.Numeric(15, 2))
-    dd_date = db.Column(db.Date)
-    court_order_dd = db.Column(db.String)
-    bank_remarks = db.Column(db.Text)
-    action_taken_by_banker = db.Column(db.Text)
+    dd_amount: Mapped[Optional[float]] = mapped_column(db.Numeric(15, 2))
+    dd_date: Mapped[Optional[date]]
+    court_order_dd: Mapped[Optional[str]]
+    bank_remarks: Mapped[Optional[str]] = mapped_column(db.Text)
+    action_taken_by_banker: Mapped[Optional[str]] = mapped_column(db.Text)
 
-    department = db.Column(db.String)
-    court_name = db.Column(db.String)
-    case_number = db.Column(db.String)
-    case_title = db.Column(db.String)
-    mact_number = db.Column(db.String)
-    petitioner_name = db.Column(db.String)
+    department: Mapped[Optional[str]]
+    court_name: Mapped[Optional[str]]
+    case_number: Mapped[Optional[str]]
+    case_title: Mapped[Optional[str]]
+    mact_number: Mapped[Optional[str]]
+    petitioner_name: Mapped[Optional[str]]
 
-    date_of_lien_order = db.Column(db.Date)
-    claim_already_paid_by_hub_office = db.Column(db.String)
-    claim_number = db.Column(db.String)
-    date_of_claim_registration = db.Column(db.Date)
-    claim_disbursement_voucher = db.Column(db.String)
+    date_of_lien_order: Mapped[Optional[date]]
+    claim_already_paid_by_hub_office: Mapped[Optional[str]]
+    claim_number: Mapped[Optional[str]]
+    date_of_claim_registration: Mapped[Optional[date]]
+    claim_disbursement_voucher: Mapped[Optional[str]]
 
-    lien_dd_reversal_order = db.Column(db.String)
-    lien_status = db.Column(db.String)
-    appeal_given = db.Column(db.String)
-    appeal_copy = db.Column(db.String)
-    appeal_given_2 = db.Column(db.String)
-    appeal_copy_2 = db.Column(db.String)
-    stay_obtained = db.Column(db.String)
-    stay_order = db.Column(db.String)
-    claim_accounting_voucher_number = db.Column(db.String)
+    lien_dd_reversal_order: Mapped[Optional[str]]
+    lien_status: Mapped[Optional[str]]
+    appeal_given: Mapped[Optional[str]]
+    appeal_copy: Mapped[Optional[str]]
+    appeal_given_2: Mapped[Optional[str]]
+    appeal_copy_2: Mapped[Optional[str]]
+    stay_obtained: Mapped[Optional[str]]
+    stay_order: Mapped[Optional[str]]
+    claim_accounting_voucher_number: Mapped[Optional[str]]
 
-    claim_accounting_voucher_date = db.Column(db.Date)
+    claim_accounting_voucher_date: Mapped[Optional[date]]
 
-    ro_remarks = db.Column(db.Text)
-    ho_tp_remarks = db.Column(db.Text)
+    ro_remarks: Mapped[Optional[str]] = mapped_column(db.Text)
+    ho_tp_remarks: Mapped[Optional[str]] = mapped_column(db.Text)
 
-    court_order_lien_reversal = db.Column(db.String)
-    court_order_dd_reversal = db.Column(db.String)
+    court_order_lien_reversal: Mapped[Optional[str]]
+    court_order_dd_reversal: Mapped[Optional[str]]
 
-    created_by = db.Column(db.String, default=lambda: current_user.username)
-    created_on = db.Column(db.DateTime, default=datetime.now)
+    created_by: Mapped[CreatedBy]
+    created_on: Mapped[CreatedOn]
 
-    updated_by = db.Column(db.String, onupdate=lambda: current_user.username)
-    updated_on = db.Column(db.DateTime, onupdate=datetime.now)
+    updated_by: Mapped[UpdatedBy]
+    updated_on: Mapped[UpdatedOn]
 
-    is_duplicate = db.Column(db.Boolean, default=False)
+    is_duplicate: Mapped[bool] = mapped_column(default=False)
 
     @validates("ro_code")
     def validate_ro_code(self, key, value):
