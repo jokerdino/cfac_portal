b0aea443ad716a7fe5cbac03dac23ed2233835ab
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-11-27 18:18:47 +0530

    BRS: Update models to sqlalchemy v2.0 style

--- app/brs/models.py
+++ app/brs/models.py
@@ -1,39 +1,41 @@
-from dataclasses import dataclass
-from datetime import datetime
+from datetime import date
+from typing import Optional
 
-from flask_login import current_user
+from flask import abort
+from sqlalchemy.orm import Mapped, mapped_column
 
-from extensions import db
+from extensions import db, IntPK, CreatedBy, CreatedOn, UpdatedBy, UpdatedOn
 
 
-@dataclass
 class BRS(db.Model):
     # TODO: Enable row level security
-    id = db.Column(db.Integer, primary_key=True)
-    uiic_regional_code: str = db.Column(db.String)
-    uiic_office_code: str = db.Column(db.String)
+    id: Mapped[IntPK]
+    uiic_regional_code: Mapped[str]
+    uiic_office_code: Mapped[str]
 
-    financial_year = db.Column(db.String)
-    month = db.Column(db.String)
+    financial_year: Mapped[str]
+    month: Mapped[str]
 
-    cash_bank: str = db.Column(db.String)
-    cheque_bank: str = db.Column(db.String)
-    pos_bank: str = db.Column(db.String)
-    pg_bank: str = db.Column(db.String)
-    bbps_bank: str = db.Column(db.String)
-    dqr_bank: str = db.Column(db.String)
-    local_collection_bank: str = db.Column(db.String)
+    cash_bank: Mapped[Optional[str]]
+    cheque_bank: Mapped[Optional[str]]
+    pos_bank: Mapped[Optional[str]]
+    pg_bank: Mapped[Optional[str]]
+    bbps_bank: Mapped[Optional[str]]
+    dqr_bank: Mapped[Optional[str]]
+    local_collection_bank: Mapped[Optional[str]]
 
-    cash_brs_id = db.Column(db.Integer)
-    cheque_brs_id = db.Column(db.Integer)
-    pos_brs_id = db.Column(db.Integer)
-    pg_brs_id = db.Column(db.Integer)
-    bbps_brs_id = db.Column(db.Integer)
-    dqr_brs_id = db.Column(db.Integer)
-    local_collection_brs_id = db.Column(db.Integer)
+    cash_brs_id: Mapped[Optional[int]]
+    cheque_brs_id: Mapped[Optional[int]]
+    pos_brs_id: Mapped[Optional[int]]
+    pg_brs_id: Mapped[Optional[int]]
+    bbps_brs_id: Mapped[Optional[int]]
+    dqr_brs_id: Mapped[Optional[int]]
+    local_collection_brs_id: Mapped[Optional[int]]
 
-    brs_month = db.relationship("BRSMonth", backref="brs", lazy="dynamic")
-    timestamp = db.Column(db.DateTime, default=datetime.now)
+    brs_month: Mapped["BRSMonth"] = db.relationship(
+        back_populates="brs", cascade="all, delete-orphan"
+    )
+    timestamp: Mapped[CreatedOn]
 
     def get_bank_for_type(self, brs_type: str) -> str:
         mapping = {
@@ -49,97 +51,104 @@ class BRS(db.Model):
 
 
 class DeleteEntries(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    txt_month = db.Column(db.String)
-    bool_enable_delete = db.Column(db.Boolean, default=True)
+    id: Mapped[IntPK]
+    txt_month: Mapped[str]
+    bool_enable_delete: Mapped[bool] = mapped_column(default=True)
 
-    created_by = db.Column(db.String, default=lambda: current_user.username)
-    created_on = db.Column(db.DateTime, default=datetime.now)
+    created_by: Mapped[CreatedBy]
+    created_on: Mapped[CreatedOn]
 
-    updated_by = db.Column(db.String, onupdate=lambda: current_user.username)
-    updated_on = db.Column(db.DateTime, onupdate=datetime.now)
+    updated_by: Mapped[UpdatedBy]
+    updated_on: Mapped[UpdatedOn]
 
 
 class BRSMonth(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    brs_id = db.Column(db.Integer, db.ForeignKey("brs.id"))
-    brs_type = db.Column(db.String)
+    id: Mapped[IntPK]
+    brs_id: Mapped[int] = mapped_column(db.ForeignKey("brs.id"))
+    brs_type: Mapped[str]
 
-    int_opening_balance = db.Column(db.Numeric(15, 2), default=0)
-    int_opening_on_hand = db.Column(db.Numeric(15, 2), default=0)
-    int_transactions = db.Column(db.Numeric(15, 2), default=0)
-    int_cancellations = db.Column(db.Numeric(15, 2), default=0)
-    int_fund_transfer = db.Column(db.Numeric(15, 2), default=0)
-    int_bank_charges = db.Column(db.Numeric(15, 2), default=0)
-    int_closing_on_hand = db.Column(db.Numeric(15, 2), default=0)
-    int_closing_balance = db.Column(db.Numeric(15, 2), default=0)
+    int_opening_balance: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_opening_on_hand: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_transactions: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_cancellations: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_fund_transfer: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_bank_charges: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_closing_on_hand: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_closing_balance: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
 
-    int_deposited_not_credited = db.Column(db.Numeric(15, 2), default=0)
-    int_short_credited = db.Column(db.Numeric(15, 2), default=0)
-    int_excess_credited = db.Column(db.Numeric(15, 2), default=0)
-
-    int_balance_as_per_bank = db.Column(db.Numeric(15, 2), default=0)
-
-    # file_outstanding_entries = db.Column(db.String)
-    timestamp = db.Column(db.DateTime, default=datetime.now)
-    status = db.Column(db.String)
-    brs_outstanding = db.relationship(
-        "Outstanding", backref="brs_month", lazy="dynamic"
+    int_deposited_not_credited: Mapped[float] = mapped_column(
+        db.Numeric(15, 2), default=0
     )
-    brs_short_credit = db.relationship(
-        "BankReconShortCredit", backref="brs_month", lazy="dynamic"
+    int_short_credited: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+    int_excess_credited: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+
+    int_balance_as_per_bank: Mapped[float] = mapped_column(db.Numeric(15, 2), default=0)
+
+    timestamp: Mapped[CreatedOn]
+    status: Mapped[Optional[str]]
+    brs_outstanding: Mapped[list["Outstanding"]] = db.relationship(
+        back_populates="brs_month", cascade="all, delete-orphan"
     )
-    brs_excess_credit = db.relationship(
-        "BankReconExcessCredit", backref="brs_month", lazy="dynamic"
+    brs_short_credit: Mapped[list["BankReconShortCredit"]] = db.relationship(
+        back_populates="brs_month", cascade="all, delete-orphan"
     )
-    remarks = db.Column(db.Text)
-    prepared_by = db.Column(db.String)
-    prepared_by_employee_number = db.Column(db.String)
+    brs_excess_credit: Mapped[list["BankReconExcessCredit"]] = db.relationship(
+        back_populates="brs_month", cascade="all, delete-orphan"
+    )
+    remarks: Mapped[Optional[str]] = mapped_column(db.Text)
+    prepared_by: Mapped[str]
+    prepared_by_employee_number: Mapped[str]
+
+    brs: Mapped["BRS"] = db.relationship(back_populates="brs_month")
+
 
 
 class Outstanding(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    brs_month_id = db.Column(db.Integer, db.ForeignKey("brs_month.id"))
+    id: Mapped[IntPK]
+    brs_month_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("brs_month.id"))
 
     # instrument number and date of instrument is optional for cash alone.
     # other modes of collection must provide all the columns
-    instrument_number = db.Column(db.String)
-    instrument_amount = db.Column(db.Numeric(15, 2))
-    date_of_instrument = db.Column(db.Date)
-    date_of_collection = db.Column(db.Date)
-    remarks = db.Column(db.Text)
+    instrument_number: Mapped[Optional[str]]
+    instrument_amount: Mapped[float] = mapped_column(db.Numeric(15, 2))
+    date_of_instrument: Mapped[Optional[date]]
+    date_of_collection: Mapped[Optional[date]]
+    remarks: Mapped[Optional[str]] = mapped_column(db.Text)
 
+    brs_month: Mapped["BRSMonth"] = db.relationship(back_populates="brs_outstanding")
 
 class BankReconShortCredit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    brs_month_id = db.Column(db.Integer, db.ForeignKey("brs_month.id"))
+    id: Mapped[IntPK]
+    brs_month_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("brs_month.id"))
 
     # instrument number and date of instrument is optional for cash alone.
     # other modes of collection must provide all the columns
-    instrument_number = db.Column(db.String)
-    instrument_amount = db.Column(db.Numeric(15, 2))
-    date_of_instrument = db.Column(db.Date)
-    date_of_collection = db.Column(db.Date)
-    remarks = db.Column(db.Text)
+    instrument_number: Mapped[Optional[str]]
+    instrument_amount: Mapped[float] = mapped_column(db.Numeric(15, 2))
+    date_of_instrument: Mapped[Optional[date]]
+    date_of_collection: Mapped[date]
+    remarks: Mapped[Optional[str]] = mapped_column(db.Text)
 
+    brs_month: Mapped["BRSMonth"] = db.relationship(back_populates="brs_short_credit")
 
 class BankReconExcessCredit(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    brs_month_id = db.Column(db.Integer, db.ForeignKey("brs_month.id"))
+    id: Mapped[IntPK]
+    brs_month_id: Mapped[int] = mapped_column(db.Integer, db.ForeignKey("brs_month.id"))
 
     # instrument number and date of instrument is optional for cash alone.
     # other modes of collection must provide all the columns
-    instrument_number = db.Column(db.String)
-    instrument_amount = db.Column(db.Numeric(15, 2))
-    date_of_instrument = db.Column(db.Date)
-    date_of_collection = db.Column(db.Date)
-    remarks = db.Column(db.Text)
+    instrument_number: Mapped[Optional[str]]
+    instrument_amount: Mapped[float] = db.Column(db.Numeric(15, 2))
+    date_of_instrument: Mapped[Optional[date]]
+    date_of_collection: Mapped[date]
+    remarks: Mapped[Optional[str]] = mapped_column(db.Text)
 
+    brs_month: Mapped["BRSMonth"] = db.relationship(back_populates="brs_excess_credit")
 
 class BankReconAccountDetails(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    str_name_of_bank = db.Column(db.String)
-    str_brs_type = db.Column(db.String)
+    id: Mapped[IntPK]
+    str_name_of_bank: Mapped[str]
+    str_brs_type: Mapped[str]
 
-    str_bank_account_number = db.Column(db.String)
-    str_ifsc_code = db.Column(db.String)
+    str_bank_account_number: Mapped[str]
+    str_ifsc_code: Mapped[str]
