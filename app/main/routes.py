77c28bdcc2bde0b1f0cdaeaacc3cec31f6f6ec08
Barneedhar Vigneshwar <barneedhar@gmail.com>
2025-12-05 22:29:57 +0530

    Main: Migrate queries to sqlalchemy 2.0 style

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,26 +1,37 @@
+from datetime import date, timedelta
+
+
 from flask import redirect, render_template, url_for
 from flask_login import current_user, login_required
 
 from app.main import main_bp
 
-# from app.tickets.tickets_routes import humanize_datetime
+
 from app.announcements.announcements_model import Announcements
 from app.mis_tracker.mis_model import MisTracker
+
+from extensions import db
 
 
 @main_bp.route("/")
 def index():
     if current_user.is_authenticated:
-        query = Announcements.query.order_by(Announcements.created_on.desc()).limit(3)
-        list = (
-            MisTracker.query.order_by(MisTracker.created_on.desc())
-            .order_by(MisTracker.created_on.desc(), MisTracker.id.asc())
-            .limit(20)
+        announcements = db.session.scalars(
+            db.select(Announcements).order_by(Announcements.created_on.desc()).limit(3)
+        )
+
+        prev_month = date.today().replace(day=1) - timedelta(days=1)
+        prev_month_string = prev_month.strftime("%B-%Y")
+
+        mis = db.session.scalars(
+            db.select(MisTracker)
+            .where(MisTracker.txt_period == prev_month_string)
+            .order_by(MisTracker.txt_mis_type.asc())
         )
         return render_template(
             "index.html",
-            query=query,
-            list=list,  # humanize_datetime=humanize_datetime,
+            announcements=announcements,
+            mis=mis,
         )
     else:
         return redirect(url_for("users.login_page"))
