858944b3ef39df44c8e601cd7649ff51e43abdb0
Barneedhar Vigneshwar <barneedhar@gmail.com>
2026-01-24 13:00:59 +0530

    Pool credits: Comment out unused code

--- app/pool_credits/pool_credits_jv.py
+++ app/pool_credits/pool_credits_jv.py
@@ -9,7 +9,7 @@ from flask import (
     url_for,
     send_file,
 )
-from flask_login import current_user, login_required
+from flask_login import login_required
 
 
 from sqlalchemy.exc import IntegrityError
@@ -18,7 +18,7 @@ from extensions import db
 from set_view_permissions import admin_required
 
 from . import pool_credits_bp
-from .pool_credits_form import JVUploadForm, PoolCreditsJVForm
+from .pool_credits_form import PoolCreditsJVForm
 from .pool_credits_model import PoolCreditsJournalVoucher, PoolCredits
 
 
@@ -69,28 +69,28 @@ def jv_edit(jv_id):
     return render_template("jv_edit.html", form=form, jv=jv, title="Edit JV mapping")
 
 
-@pool_credits_bp.route("/jv/bulk_upload/", methods=["GET", "POST"])
-@login_required
-@admin_required
-def jv_bulk_upload():
-    form = JVUploadForm()
-    if form.validate_on_submit():
-        jv_file = form.jv_file.data
-        df_jv = pd.read_excel(
-            jv_file,
-            dtype={"str_regional_office_code": str},
-        )
-        df_jv["created_on"] = datetime.now()
-        df_jv["created_by"] = current_user.username
-        df_jv.to_sql(
-            "pool_credits_journal_voucher",
-            db.engine,
-            if_exists="append",
-            index=False,
-        )
+# @pool_credits_bp.route("/jv/bulk_upload/", methods=["GET", "POST"])
+# @login_required
+# @admin_required
+# def jv_bulk_upload():
+#     form = JVUploadForm()
+#     if form.validate_on_submit():
+#         jv_file = form.jv_file.data
+#         df_jv = pd.read_excel(
+#             jv_file,
+#             dtype={"str_regional_office_code": str},
+#         )
+#         df_jv["created_on"] = datetime.now()
+#         df_jv["created_by"] = current_user.username
+#         df_jv.to_sql(
+#             "pool_credits_journal_voucher",
+#             db.engine,
+#             if_exists="append",
+#             index=False,
+#         )
 
-        return redirect(url_for("pool_credits.jv_list"))
-    return render_template("jv_bulk_upload.html", form=form)
+#         return redirect(url_for("pool_credits.jv_list"))
+#     return render_template("jv_bulk_upload.html", form=form)
 
 
 # @pool_credits_bp.route("/download_jv/")
