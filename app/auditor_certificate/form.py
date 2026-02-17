0285a09b9e51a45157b409b887a49741cb01523d
Barneedhar Vigneshwar <barneedhar@gmail.com>
2026-02-17 12:32:09 +0530

    Audit certificate: Updates to form labels and remove none in list template

--- app/auditor_certificate/form.py
+++ app/auditor_certificate/form.py
@@ -5,21 +5,23 @@ from wtforms.validators import DataRequired, Optional
 
 
 class AuditorCertificateForm(FlaskForm):
-    ro_code = StringField(validators=[DataRequired()])
-    ro_name = StringField(validators=[DataRequired()])
+    ro_code = StringField("RO code", validators=[DataRequired()])
+    ro_name = StringField("RO name", validators=[DataRequired()])
 
     purpose = TextAreaField(validators=[DataRequired()])
-    date_of_request = DateField(validators=[DataRequired()])
+    date_of_request = DateField("Date of request", validators=[DataRequired()])
 
-    bid_closing_date = DateField(validators=[Optional()])
-    certificate_issued_date = DateField(validators=[Optional()])
+    bid_closing_date = DateField("Bid closing date", validators=[Optional()])
+    certificate_issued_date = DateField(
+        "Certificate issued date", validators=[Optional()]
+    )
 
-    invoice_received_date = DateField(validators=[Optional()])
-    invoice_date = DateField(validators=[Optional()])
+    invoice_received_date = DateField("Invoice received date", validators=[Optional()])
+    invoice_date = DateField("Invoice date", validators=[Optional()])
 
     remarks = TextAreaField(validators=[Optional()])
 
-    disbursement_date = DateField(validators=[Optional()])
-    request_id = StringField(validators=[Optional()])
-    date_of_payment = DateField(validators=[Optional()])
-    utr_number = StringField(validators=[Optional()])
+    disbursement_date = DateField("Disbursement date", validators=[Optional()])
+    request_id = StringField("Request ID", validators=[Optional()])
+    date_of_payment = DateField("Date of payment", validators=[Optional()])
+    utr_number = StringField("UTR number", validators=[Optional()])
